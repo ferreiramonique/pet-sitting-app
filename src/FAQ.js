1b713f19c4313fd6ac4e5c0987eb28167a5d8c2b
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2023-06-26 23:37:41 +0100

    refactor: change source code link display in FAQ

--- src/FAQ.js
+++ src/FAQ.js
@@ -9,6 +9,10 @@ function FAQ() {
       {faqDatabase.data.map(( el ) => (
         <Accordion title={el.title} content={el.content} />
       ))}
+      <Accordion title={"I am a developer. Where is the source code for this website?"}
+      content={
+        <a className="link" href="https://github.com/mgrinstein/pet-sitting-app" target="_blank">View Source Code</a>
+      } />
     </div>
   );
 }
